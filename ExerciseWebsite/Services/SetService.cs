3afd6b24463f574c0c96ac2f4916f2d1cda70b89
nrholm1 <nielsraunkjaer@gmail.com>
2020-08-03 22:02:45 +0200

    Removed method that accidentally got copy pasted

--- ExerciseWebsite/Services/SetService.cs
+++ ExerciseWebsite/Services/SetService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExerciseWebsite.Entities;
+using ExerciseWebsite.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +9,50 @@ namespace ExerciseWebsite.Services
 {
     public interface ISetService
     {
-
+        Set Create(Set set);
+        IEnumerable<Set> GetAll();
+        IEnumerable<Set> GetBySetListId(int setListId);
+        Set GetById(int id);
+        void Update(Set set);
+        void Delete(Set set);
     }
     public class SetService : ISetService
     {
+        private DataContext _context;
+
+        public SetService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Set Create(Set set)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IEnumerable<Set> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Set GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Set> GetBySetListId(int setListId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Set set)
+        {
+            throw new NotImplementedException();
+        }
+        public void Delete(Set set)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
